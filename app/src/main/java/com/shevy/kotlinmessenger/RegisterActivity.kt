380b5399c113b0e48af8d8426d34e03991e3173b
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-01 10:18:15 +0500

    Added LatestMessagesActivity

--- app/src/main/java/com/shevy/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/shevy/kotlinmessenger/RegisterActivity.kt
@@ -131,6 +131,10 @@ class RegisterActivity : AppCompatActivity() {
         ref.setValue(user)
             .addOnSuccessListener {
                 Log.d("Register", "Finally we saves the user to Firebase Database")
+
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
             }
     }
 }
